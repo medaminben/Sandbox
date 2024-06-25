62b61d603638d10855c2f640939d06d80b38a890
medaminben <mohamed.ben-hassine@mailfence.com>
2024-06-25 02:54:30 +0200

    the result template

--- libraries/Core/include/Sandbox/Core/Core.h
+++ libraries/Core/include/Sandbox/Core/Core.h
@@ -1,13 +1,29 @@
 #ifndef CORE_H
 #define CORE_H
 #include "Sandbox_export.h"
+#include <variant>
+/**
+ * @brief  a result wrapper
+ * 
+ * @tparam VT  a value type
+ * @tparam ET  an error type 
+ */
+template <typename VT, typename ET>
+struct Result {
+public:
+    constexpr Result(VT value): result(std::move(value)) {};
+    constexpr auto isValue() const { return std::holds_alternative<VT>(result); };
+    constexpr auto&& operator()()  { return std::move(std::get<VT>(result)); };
 
+    constexpr Result(ET error): result(std::move(error)) {};
+    constexpr auto isError() const { return std::holds_alternative<ET>(result); };
+    constexpr auto&& error()       { return std::move(std::get<ET>(result)); };
+private:
+    std::variant<VT, ET> result;
+};
 namespace Sandbox {
     namespace Core {
-        unsigned int SANDBOX_API get_the_answer(void) ;
-
-        int SANDBOX_API multiply(int, int) ;
     }
 }
-
+namespace Sc = Sandbox::Core;
 #endif // CORE_H
--- libraries/Core/test/src/test_Core.cpp
+++ libraries/Core/test/src/test_Core.cpp
@@ -5,12 +5,7 @@
 #include <iostream>
 #include <string>
 
-// Test case names must start with the name of the executable they are
-// contained in, in this case, "test_core"
-TEST (test_Core_Math, AnswerIsCorrect) {
-    EXPECT_EQ(Sandbox::Core ::get_the_answer(), 42U);
-}
-
-TEST (test_Core_Math, MultiplyReturnsCorrectResults) {
-    EXPECT_EQ(36, Sandbox::Core::multiply(6,6));
+TEST(test_Core_result, is_result) {
+    auto result = Result<int, std::string>(5);
+    EXPECT_EQ(result(), 5);
 }
