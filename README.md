c82334ae8a401bdce803473a5c1b1fafb5c6ac61
medaminben <mohamed.ben-hassine@mailfence.com>
2024-06-25 02:46:54 +0200

    add the generated Core library from the boilerplate

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Sandbox
 a practice repo based on [project-unit](https://github.com/medaminben/project-unit) repository
 
+
+## Libraries
+### Core
+The core library defines the common code base such as containers and custom types to ensure a manageable system
+The base unit of the system is a value. The system at any moment of the life cycle is not doing other than managing values, but the result of the processing can be errors in exception and corner cases: 
+- Value any rValue type
+- Error 
+- Result<Value,Error>  
--- cmake/GTestSupport.cmake
+++ cmake/GTestSupport.cmake
@@ -1,9 +1,29 @@
 include(FetchContent)
 # clone gtest build and install it 
+#set(FETCHCONTENT_BASE_DIR "${3RD_PARTY_DIR}/deps")
+
+set(LIBRARY_NAME googletest)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG main
+  SOURCE_DIR    "${3RD_PARTY_DIR}/googletest/src"
+  BINARY_DIR    "${3RD_PARTY_DIR}/googletest/build"
+  SUBBUILD_DIR  "${3RD_PARTY_DIR}/googletest/build"
+  TMP_DIR       "${3RD_PARTY_DIR}/googletest/build"
 )
-FetchContent_Makeavailable(googletest)
+FetchContent_GetProperties(googletest)
+if(NOT googletest_POPULATED)
+    FetchContent_Populate(googletest)
+    add_subdirectory("${3RD_PARTY_DIR}/googletest/src" 
+                     "${3RD_PARTY_DIR}/googletest/build")  
+    set_target_properties(gmock gmock_main gtest gtest_main
+      PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY     ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+      LIBRARY_OUTPUT_DIRECTORY     ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+      ARCHIVE_OUTPUT_DIRECTORY     ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
+      PDB_OUTPUT_DIRECTORY         ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+      COMPILE_PDB_OUTPUT_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+    )
+endif()
 include(googletest)
