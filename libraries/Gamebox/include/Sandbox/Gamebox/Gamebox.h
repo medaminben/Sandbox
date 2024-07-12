3482fded1763ea8a6253ae88e1ca87510f133037
medaminben <mohamed.ben-hassine@mailfence.com>
2024-07-12 06:12:34 +0200

    fix the export file for the gamebox lib

--- libraries/Gamebox/include/Sandbox/Gamebox/Gamebox.h
+++ libraries/Gamebox/include/Sandbox/Gamebox/Gamebox.h
@@ -1,28 +1,26 @@
 #ifndef GAMEBOX_H
 #define GAMEBOX_H
-#include "Sandbox_export.h"
+#include "Sandbox_Gamebox_export.h"
 #include <memory>
 struct BowlingSet;
-namespace Sandbox {
-    namespace Gamebox {
+namespace Sandbox { namespace Gamebox {
         
-        class Bowlingbox {
-            public:
-                SANDBOX_API Bowlingbox();
-                SANDBOX_API ~Bowlingbox();
-            public:
-                void SANDBOX_API roll(const int&);
-                int  SANDBOX_API score();
-                int* SANDBOX_API rolls();
-                int* SANDBOX_API scores();
-                int  SANDBOX_API pinsLeft();
-                int  SANDBOX_API getFrameScore(const int&);
-                void SANDBOX_API reset();
-                bool SANDBOX_API isOver();
-            private:
-                std::unique_ptr<BowlingSet> _gameSet;
-        };
-    }
-}
+   class Bowlingbox {
+        public:
+            SANDBOX_GAMEBOX_API Bowlingbox();
+            SANDBOX_GAMEBOX_API ~Bowlingbox();
+        public:
+            void SANDBOX_GAMEBOX_API roll(const int&);
+            int  SANDBOX_GAMEBOX_API score();
+            int* SANDBOX_GAMEBOX_API rolls();
+            int* SANDBOX_GAMEBOX_API scores();
+            int  SANDBOX_GAMEBOX_API pinsLeft();
+            int  SANDBOX_GAMEBOX_API getFrameScore(const int&);
+            void SANDBOX_GAMEBOX_API reset();
+            bool SANDBOX_GAMEBOX_API isOver();
+        private:
+            std::unique_ptr<BowlingSet> _gameSet;
+    };
+}}
 
 #endif // GAMEBOX_H
