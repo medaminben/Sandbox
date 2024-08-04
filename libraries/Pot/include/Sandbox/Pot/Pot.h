8a1de1768a5c48f7869fb9c5086a1dcf3ccf0166
medaminben <mohamed.ben-hassine@mailfence.com>
2024-08-05 01:45:19 +0200

    create INIfile in Pot as ini data wrapper

--- libraries/Pot/include/Sandbox/Pot/Pot.h
+++ libraries/Pot/include/Sandbox/Pot/Pot.h
@@ -1,12 +1,15 @@
 #ifndef POT_H
 #define POT_H
+#include "INIfile.h"
 
-namespace Sandbox {
-    namespace Pot {
-        auto  get_the_answer() {return 42U;};
+namespace Sandbox { namespace Pot {
+    int  inline  get_the_answer() {return 42U;};
+    int  inline  multiply(int l, int r) { return l*r;};
 
-        auto  multiply(int l, int r) { return l*r;};
-    }
-}
+}}
 
+
+namespace Sp = Sandbox::Pot;
 #endif // POT_H
+
+
