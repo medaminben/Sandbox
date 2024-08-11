858ebb8db23a3a04ef8e4e526f3f69d3b1022047
medaminben <mohamed.ben-hassine@mailfence.com>
2024-08-11 12:19:17 +0200

    compile time vs run time illustration

---
+++ applications/designPattern/ct_vs_rt/README.md
@@ -0,0 +1,1 @@
+Illustration of the difference between compiler time and run time function initialization
---
+++ applications/designPattern/ct_vs_rt/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+int run_time_get_function() { return 10;}
+
+// constexpr: the expression should be evaluated at compile time
+constexpr int compile_time_get_function() { return 10;}
+
+void compile_time_function() {
+    std::cout << "\n -------------- Compile time initialization" << "\n";
+
+    const int n = compile_time_get_function();
+    int* ptr = (int*)(&n);
+
+    *ptr = 0;
+    std::cout <<
+    "n =          " << std::setw(3) << n           << ",\t &n  = "  <<&n   << "\n" <<
+    "*(&n) =      " << std::setw(3) << *(&n)       << ",\t &n  = "  <<&n   << "\n" <<
+    "*((int*)&n) =" << std::setw(3) << *((int*)&n) << ",\t &n  = "  <<&n   << "\n" <<
+    "*ptr =       " << std::setw(3) << *ptr        << ",\t ptr = " << ptr << "\n" <<
+    "\n";
+}
+void run_time_function() {
+    std::cout << "\n -------------- Run time initialization" << "\n";
+
+    const int n = run_time_get_function();
+    int* ptr = (int*)(&n);
+
+    *ptr = 0;
+    std::cout <<
+    "n =          " << std::setw(3) << n           << ",\t &n  = "  <<&n   << "\n" <<
+    "*(&n) =      " << std::setw(3) << *(&n)       << ",\t &n  = "  <<&n   << "\n" <<
+    "*((int*)&n) =" << std::setw(3) << *((int*)&n) << ",\t &n  = "  <<&n   << "\n" <<
+    "*ptr =       " << std::setw(3) << *ptr        << ",\t ptr = " << ptr << "\n" <<
+    "\n";
+}
+int main () {
+   run_time_function();
+   compile_time_function();
+   return 0;
+}
