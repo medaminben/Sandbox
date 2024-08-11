b40418623bed124982bc31a66495fe18085756c4
medaminben <mohamed.ben-hassine@mailfence.com>
2024-08-11 03:27:31 +0200

    accessibility constraints
    use a boolean rule to find out if the argument are appropriate or not

--- libraries/Pot/include/Sandbox/Pot/Conception.h
+++ libraries/Pot/include/Sandbox/Pot/Conception.h
@@ -2,44 +2,33 @@
 #define CONCEPTION_H
 #include <type_traits>
 #include <concepts>
-#include <tuple>
+#include <cmath>
+namespace Sandbox { namespace Pot { 
 
-namespace Sandbox { namespace Pot {
 //concepts
  /**
  * @brief Numeric concept 
  * template type guard for numeric types
- * @tparam T type to assert
+ * @param numeric_type type to assert
  */
-template<typename T> concept Numeric = requires(T param) 
+template<class numeric_type> concept Numeric = requires(numeric_type param) 
 {
-    requires  std::is_integral_v<T> || std::is_floating_point_v<T>;
-    requires !std::is_same_v<bool, T>;
+    requires  std::is_integral_v<numeric_type> 
+           || std::is_floating_point_v<numeric_type>;
+    requires !std::is_same_v<bool, numeric_type>;
     requires  std::is_arithmetic_v<decltype(param +1)>;
-    requires !std::is_pointer_v<T>;
+    requires !std::is_pointer_v<numeric_type>;
 };
 
-/**
- * @brief trait
- * 
- * @tparam P 
- * @tparam D 
- */
-template <typename P, int D> struct access;
+// Rule Constraints
+template<bool> struct rule       : std::false_type {};
+template<>     struct rule<true> : std::true_type  {};
 
-template <typename P> struct access<P, 0> { 
-    static constexpr Numeric auto get(P const& p) noexcept { return p.x; }  
-};
-template <typename P> struct access<P, 1> { 
-    static constexpr Numeric auto get(P const& p) noexcept { return p.y; }  
-};
-template <typename P> struct access<P, 2> { 
-    static constexpr Numeric auto get(P const& p) noexcept { return p.z; }  
-};
-
-
-template <int D, typename P> 
-constexpr auto get(P const& p) { return access<P, D>::get(p); } 
+// accessibility constraint
+// if the dimension is higher than the space boundary 
+// doesn't create (example of use see Point.h)
+template<size_t dimension, size_t point_size> 
+struct is_accessible : rule<(dimension < point_size)>{};
 
 }}
 
